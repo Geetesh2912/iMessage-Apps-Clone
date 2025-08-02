b6f0183975641182bb1d09b66fadab07bb762aa2
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2025-08-03 02:26:23 +0530

    Added view class for representing the cell on Messages Category Screen on HomeVC

---
+++ iMessage-Geet/iMessage-Geet/Views/MessageCategoryCell.swift
@@ -0,0 +1,40 @@
+//  Created by Geetesh Mandaogade on 02/08/25.
+
+import UIKit
+import Foundation
+
+class MessageCategoryCell: UITableViewCell {
+
+    // MARK: - Properties
+
+    static let reuseIdentifier = "MessageCategoryCell"
+    private lazy var messageCategoryView = MessageCategoryView()
+
+    // MARK: - Initializers
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(messageCategoryView)
+        messageCategoryView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            messageCategoryView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            messageCategoryView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            messageCategoryView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            messageCategoryView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Public API
+
+    func configure(icon: UIImage, title: String, unreadMessagesCount: Int?) {
+        messageCategoryView.configureView(
+            with: icon,
+            title: title,
+            unreadMessagesCount: unreadMessagesCount)
+    }
+}
