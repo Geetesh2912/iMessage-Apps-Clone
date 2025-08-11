62dd91fd48b0862e365a12ec045fdcf3a1a126d2
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2025-08-12 00:24:45 +0530

    Issue: Table view cell looks “stuck” in its selected state after coming back to the Categories/Filter screen
    Fix:
    - Added implementation to deselect tableView cell just after it was activated.

--- iMessage-Geet/iMessage-Geet/ViewControllers/MessageCategoriesViewController.swift
+++ iMessage-Geet/iMessage-Geet/ViewControllers/MessageCategoriesViewController.swift
@@ -74,6 +74,7 @@ extension MessageCategoriesViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
         let messageCategory = MessageCategoriesMock.sections[indexPath.section].categories[indexPath.row]
 
         let newViewController = MessagesListViewController(
